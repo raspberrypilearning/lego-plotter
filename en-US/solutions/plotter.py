8a2831fbb09cb2315db98bdfefc1788616ec675f
majamanojlovic <49232422+majamanojlovic@users.noreply.github.com>
2021-10-21 15:21:46 +0100

    New translations plotter.py (English)

---
+++ en-US/solutions/plotter.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+from random import randint
+from time import sleep
+from buildhat import Motor, ForceSensor
+from vcgencmd import Vcgencmd
+
+button = ForceSensor('C')
+motor_y = Motor('A')
+motor_x = Motor('B')
+vcgm = Vcgencmd()
+
+def remap(min_temp, max_temp, min_angle, max_angle, temp):
+    temp_range = (max_temp - min_temp)
+    motor_range = (max_angle - min_angle)
+    mapped = (((temp - min_temp) * motor_range) / temp_range) + min_angle
+    return int(mapped)
+
+motor_y.run_to_position(0, 100)
+motor_x.start(speed=-25)
+
+while not button.is_pressed():
+    temp = vcgm.measure_temp()
+    current_angle = motor_y.get_aposition()
+    new_angle = remap(53, 57, -170, 170, temp)
+    print(f'temp is {temp} current_angle is {current_angle} new_angle is {new_angle}')
+    if new_angle > current_angle:
+        motor_y.run_to_position(new_angle, 100, direction="clockwise")
+        print('Turning CW')
+    elif new_angle < current_angle:
+        motor_y.run_to_position(new_angle, 100, direction="anticlockwise")
+        print('Turning ACW')
+    sleep(0.1)
+    
+motor_x.stop()
+motor_y.run_to_position(0, 100)
