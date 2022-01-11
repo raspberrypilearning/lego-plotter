29dfe2205b60926a000d57a70fcc58cce0b2e00a
majamanojlovic <49232422+majamanojlovic@users.noreply.github.com>
2022-01-11 18:16:59 +0000

    New translations plotter.py (German)

--- de-DE/solutions/plotter.py
+++ de-DE/solutions/plotter.py
@@ -4,31 +4,31 @@ from time import sleep
 from buildhat import Motor, ForceSensor
 from vcgencmd import Vcgencmd
 
-button = ForceSensor('C')
 motor_y = Motor('A')
 motor_x = Motor('B')
+taster = ForceSensor('C')
 vcgm = Vcgencmd()
 
-def remap(min_temp, max_temp, min_angle, max_angle, temp):
-    temp_range = (max_temp - min_temp)
-    motor_range = (max_angle - min_angle)
-    mapped = (((temp - min_temp) * motor_range) / temp_range) + min_angle
-    return int(mapped)
+def umwandlung(min_temp, max_temp, min_winkel, max_winkel, temp):
+    temp_bereich = (max_temp - min_temp)
+    motor_bereich = (max_winkel - min_winkel)
+    gewandelt = (((temp - min_temp) * motor_bereich) / temp_bereich) + min_winkel
+    return int(gewandelt)
 
 motor_y.run_to_position(0, 100)
 motor_x.start(speed=-25)
 
-while not button.is_pressed():
+while not taster.is_pressed():
     temp = vcgm.measure_temp()
-    current_angle = motor_y.get_aposition()
-    new_angle = remap(53, 57, -170, 170, temp)
-    print(f'temp is {temp} current_angle is {current_angle} new_angle is {new_angle}')
-    if new_angle > current_angle:
-        motor_y.run_to_position(new_angle, 100, direction="clockwise")
-        print('Turning CW')
-    elif new_angle < current_angle:
-        motor_y.run_to_position(new_angle, 100, direction="anticlockwise")
-        print('Turning ACW')
+    winkel_jetzt = motor_y.get_aposition()
+    sensor_daten = umwandlung(53, 57, -170, 170, temp)
+    print(f'temp is {temp} Motorstellung ist {winkel_jetzt} sensor-wert ist {sensor-daten}')
+    if sensor_daten > winkel_jetzt:
+        motor_y.run_to_position(sensor_daten, 100, direction="clockwise")
+        print('im Uhrzeigersinn')
+    elif sensor_daten < winkel_jetzt:
+        motor_y.run_to_position(sensor_daten, 100, direction="anticlockwise")
+        print('gegen Uhrzeigersinn')
     sleep(0.1)
     
 motor_x.stop()
